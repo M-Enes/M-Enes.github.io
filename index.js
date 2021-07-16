ed9f484a4b058f1fad11681256f98cbb70d4d61b
M-Enes <karaca.1453.27@gmail.com>
2021-07-16 14:37:33 +0300

    Reverting to 7e38b45

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const langButton = document.getElementById('lang-button');
 const heroContent = document.getElementsByClassName('hero-content').item(0);
 const workingHeader = document.getElementById('working').children.item(0).children.item(0);
 const cardBodies = document.getElementsByClassName("card-body");
+const linkbuttons = document.getElementsByClassName('link-button');
 const contactHeader = document.getElementById('contact').children.item(0).children.item(0);
 
 function changeLang(){
@@ -23,7 +24,11 @@ function changeLang(){
         cardBodies.item(0).innerHTML = "Saf Python biliyorum. Python kullanarak işlerimi otomatikleştiren kodlar yazıyorum.";
         cardBodies.item(1).innerHTML = "C#'ı seviyorum. C# ile Blazor uygulamaları veya MVC websiteleri gibi projeler geliştiriyorum.";
         cardBodies.item(2).innerHTML = "Html, Css ve JavaScript web geliştirme için gereklidir. Bunları öğrenmeye web projeleri ile devam ediyorum.";
-        cardBodies.item(3).innerHTML = "Node.js bir JavaScript çalışma zamanıdır. Node.js ve TypeScript ile projeler yapıyorum.";
+        cardBodies.item(3).innerHTML = "C++ popüler ve yaygın olarak kullanılan bir programlama dilidir. Şu an C++ öğreniyorum.";
+        linkbuttons.item(0).children.item(0).innerHTML = "Çalışmaları Gör"
+        linkbuttons.item(1).children.item(0).innerHTML = "Çalışmaları Gör"
+        linkbuttons.item(2).children.item(0).innerHTML = "Çalışmaları Gör"
+        linkbuttons.item(3).children.item(0).innerHTML = "Çalışmaları Gör"
         contactHeader.innerHTML = "İletişim";
     }
     else {
@@ -40,7 +45,11 @@ function changeLang(){
         cardBodies.item(0).innerHTML = "I know pure Python. I write a code using Python, that automates my works.";
         cardBodies.item(1).innerHTML = "I love C#. I'm developing new projects with him like Blazor Apps or MVC websites.";
         cardBodies.item(2).innerHTML = "Html, Css and JavaScript are necessary for web development.I continue learn these with the web projects.";
-        cardBodies.item(3).innerHTML = "Node.js is a Javascript runtime. I make a projects with Node.js and TypeScript.";
+        cardBodies.item(3).innerHTML = "C++ is a popular and widely used programming language. I learning C++ now.";
+        linkbuttons.item(0).children.item(0).innerHTML = "Learn More"
+        linkbuttons.item(1).children.item(0).innerHTML = "Learn More"
+        linkbuttons.item(3).children.item(0).innerHTML = "Learn More"
+        linkbuttons.item(2).children.item(0).innerHTML = "Learn More"
         contactHeader.innerHTML = "Contact";
     }
 }
